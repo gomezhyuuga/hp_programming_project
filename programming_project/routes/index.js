7f827e71c0d1f9a4600a4e9e4fb1cf2a906c98ea
Fernando Gómez <gomezhyuuga@gmail.com>
2016-05-20 17:12:11 -0500

    Route to get txt file

--- programming_project/routes/index.js
+++ programming_project/routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
+var exec    = require('child_process').execFileSync;
+var fs      = require('fs');
 var router  = express.Router();
 
+function create_histogram(key) {
+  exec('generator', [key], function (error, stdout, stderr) {
+    console.log(stdout);
+    if (error) return res.sendStatus(500);
+  });
+}
+
+/* GET HISTOGRAM BY KEY */
+router.get('/get', function(req, res, next) {
+  var key = 1;
+  var filename = key + '.txt';
+  fs.stat(filename, function(err, stats) {
+    if (err) {
+      // Histogram doesn't exist. Create.
+      console.log('Creating histogram ' + key + '...');
+    }
+
+    var data = fs.readFileSync(filename);
+
+    res.send(data);
+  });
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // GET KEY of histogram
+  res.render('index', { output: 'hi' });
 });
 
 module.exports = router;
